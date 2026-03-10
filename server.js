869a7f33a86a5781bf709a971e891742ae7343ea
jongjong0514 <dlwhdghk1259@naver.com>
2026-03-10 21:36:43 +0900

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello Azure!");
+  res.send("Azure Deploy Success!");
 });
 
 app.listen(PORT, () => {
